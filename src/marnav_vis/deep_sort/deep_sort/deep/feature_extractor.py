ebccc8cead785af8bd342b63ebedfde06d1e436f
bscren <mayuxuan61@gmail.com>
2026-01-04 13:58:12 +0800

    简化代码包：删除本地不需要的功能包

--- src/marnav_vis/deep_sort/deep_sort/deep/feature_extractor.py
+++ src/marnav_vis/deep_sort/deep_sort/deep/feature_extractor.py
@@ -61,7 +61,7 @@ class Extractor(object):
 
 
 # 作者的原码：
-# if __name__ == '__main__':
+if __name__ == '__main__':
     img = cv2.imread("demo.jpg")[:,:,(2,1,0)]
     extr = Extractor("checkpoint/ckpt.t7")
     feature = extr(img)
